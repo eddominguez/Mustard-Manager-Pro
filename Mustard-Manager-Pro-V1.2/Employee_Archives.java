ac0a14f390aa6f2b4c41a73318168be24ca21523
abloomquist8 <55147054+abloomquist8@users.noreply.github.com>
2020-04-16 15:05:59 -0500

    Add files via upload

---
+++ Mustard-Manager-Pro-V1.2/Employee_Archives.java
@@ -0,0 +1,21 @@
+package fourSixteen;
+import java.util.*;
+
+
+public class Employee_Archives {
+	private ArrayList<Employee_Archive> archives;
+	public Employee_Archives() {
+		archives = new ArrayList<>();
+	}
+	public void addArchive(Employee_Archive archive) {
+		archives.add(Employee_Archive);
+	}
+	
+	public ArrayList<Employee_Archive> getEmployee_Archives()
+	{
+		return archives;
+	}
+	public void setEmployee_Archives(ArrayList<Employee_Archive> archives) {
+		this.archives = archives;
+	}
+}
